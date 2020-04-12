78f419722c4e689d7da5c4c119d32c10ed17220e
Ricardo <ricardotavares51@hotmail.com>
2020-04-11 21:28:05 -0300

    Versão 0.4.1
    Ajuste no formulário: Criação da validação;
    Ajuste do showModalBottomSheet: O formulário acompanha o teclado, ou seja, ele sobe junto quando  o teclado é aberto

--- tak/lib/Pages/AddItemPage/AddItemPage.dart
+++ tak/lib/Pages/AddItemPage/AddItemPage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:tak/Theme/theme.dart';
 import 'package:tak/Objects/Item.dart';
@@ -9,6 +11,9 @@ class AddItemPage extends StatefulWidget {
 
 class _AddItemPageState extends State<AddItemPage> {
   final _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
+  int _amount;
+  Item _itemSelected;
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +33,12 @@ class _AddItemPageState extends State<AddItemPage> {
           return ListTile(
             title: Text(item.name, style: title_item),
             trailing: Text('R\$ ' +  item.price.toStringAsFixed(2).replaceAll('.', ','), style: subtotal_text),
-            onTap: showAmountForm,
+            onTap: (){
+              showAmountForm();
+              setState(() {
+                this._itemSelected = item;
+              });   
+            },
           );
         }, 
         separatorBuilder: (context, index){
@@ -45,12 +55,15 @@ class _AddItemPageState extends State<AddItemPage> {
   showAmountForm(){
     showModalBottomSheet(
       context: context, 
+      isScrollControlled: true,
       builder: (BuildContext context){
         return StatefulBuilder(
           builder: (BuildContext context, setState){
             return Container(
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
               child: Form(
                 key: this._formKey,
+                autovalidate: this._autoValidate,
                 child: Container(
                   padding: EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 10),
                   child: Column(
@@ -61,6 +74,17 @@ class _AddItemPageState extends State<AddItemPage> {
                         decoration: InputDecoration(
                           labelText: 'Informe a quantidade'
                         ),
+                        validator: (String val){
+                          try{
+                            int.parse(val);
+                            return null;
+                          }catch(e){
+                            return 'Quantidade Inválida';
+                          }
+                        },
+                        onSaved: (String val){
+                          this._amount = int.parse(val);
+                        },
                       ),
                       SizedBox(height: 20,),
                       Row(
@@ -74,7 +98,7 @@ class _AddItemPageState extends State<AddItemPage> {
                                   borderRadius: BorderRadius.circular(7),
                                 ),
                                 child: Text('Adicionar', style: button_text),
-                                onPressed: (){},
+                                onPressed: this._saveForm,
                               ),
                             )
                           )
@@ -89,5 +113,17 @@ class _AddItemPageState extends State<AddItemPage> {
         );
       }
     );
+  }
+  void _saveForm() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      sleep(const Duration(milliseconds: 500));
+      if((this._itemSelected != null) && (this._amount != null)){ print("\n Pode Criar"); }
+      Navigator.pop(context);
+    } else {
+      setState(() {
+        _autoValidate = true;
+      });
+    }
   }
 }
