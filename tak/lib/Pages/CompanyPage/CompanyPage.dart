3315d09127f489a461f43663af390bd32bec9f32
Ricardo <ricardotavares51@hotmail.com>
2020-04-19 19:39:20 -0300

    Versão 0.5.2
    Agora os validadores estão em um arquivo separado;
    Mudança no tema do appbar do CompanyPage;
    Colocação do CircleAvatar do perfil para depois inserir o image picker

--- tak/lib/Pages/CompanyPage/CompanyPage.dart
+++ tak/lib/Pages/CompanyPage/CompanyPage.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:tak/Controllers/CompanyPageControllers/CompanyController.dart';
+import 'package:tak/Functions/Validators.dart' as Validators;
 import 'package:tak/Theme/theme.dart';
 
 class CompanyPage extends StatefulWidget {
@@ -36,14 +37,12 @@ class _CompanyPageState extends State<CompanyPage>{
       builder: (context, snapshot){
         return Scaffold(
           appBar: AppBar(
-            backgroundColor: Color(0x00000000),
-            iconTheme: IconThemeData(
-              color: primary_color, 
-            ),
-            elevation: 0,
+            backgroundColor: primary_color,
+            centerTitle: true,
+            title: Text('Perfil', style: app_bar),
             actions: <Widget>[
               IconButton(
-                icon: Icon(this._controller.getIcon(), color: primary_color, size: 30,),
+                icon: Icon(this._controller.getIcon(), color: background_color, size: 30,),
                 onPressed: (){
                   if(this._controller.getEditMode()){
                     if(this._controller.submit()){
@@ -67,80 +66,75 @@ class _CompanyPageState extends State<CompanyPage>{
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundImage: AssetImage('images/profile.png'),
+                  ),
+                  SizedBox(height: 5,),
                   TextFormField(
                     enabled: this._controller.getEditMode(),
-                    //style: TextStyle(color: background_color),
                     decoration: InputDecoration(
                       icon: Icon(MdiIcons.account, size: 30, color: decoration_color),
                       labelText: 'Nome Fantasia',
-                      //labelStyle: TextStyle(color: background_color),
                       errorStyle: TextStyle(color: danger_color),
                     ),
                     initialValue: this._controller.getName(),
                     onSaved: (String value){this._controller.setName(value);},
-                    validator: this._controller.nameValidator,
+                    validator: Validators.nameValidator,
                   ),
 
                   TextFormField(
                     enabled: this._controller.getEditMode(),
-                    //style: TextStyle(color: background_color),
                     keyboardType: TextInputType.phone,
                     decoration: InputDecoration(
                       icon: Icon(MdiIcons.badgeAccountHorizontalOutline, size: 30, color: decoration_color),
                       labelText: 'CNPJ',
-                     // labelStyle: TextStyle(color: background_color),
                       errorStyle: TextStyle(color: danger_color),
                     ),
                     initialValue: this._controller.getCNPJ(),
                     maxLength: 14,
                     onSaved: (String value){this._controller.setCNPJ(value);},
-                    validator: this._controller.cnpjValidator,  
+                    validator: Validators.cnpjValidator,  
                   ),
 
                   TextFormField(
                     enabled: this._controller.getEditMode(),
-                    //style: TextStyle(color: background_color),
                     keyboardType: TextInputType.phone,
                     decoration: InputDecoration(
                       icon: Icon(MdiIcons.phone, size: 30, color: decoration_color),
                       labelText: 'Número de Telefone',
-                     // labelStyle: TextStyle(color: background_color),
                       errorStyle: TextStyle(color: danger_color),
                     ),
                     initialValue: this._controller.getPhoneNumber(),
                     maxLength: 11,
                     onSaved: (String value){this._controller.setPhoneNumber(value);},
-                    validator: this._controller.phoneValidator,
+                    validator: Validators.phoneValidator,
                   ),
 
                   TextFormField(
                     enabled: this._controller.getEditMode(),
-                    //style: TextStyle(color: background_color),
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       icon: Icon(MdiIcons.at, size: 30, color: decoration_color),
                       labelText: 'Email',
-                    //  labelStyle: TextStyle(color: background_color),
                       errorStyle: TextStyle(color: danger_color),
                     ),
                     initialValue: this._controller.getEmail(),
                     onSaved: (String value){this._controller.setEmail(value);},
-                    validator: this._controller.emailValidator,
+                    validator: Validators.emailValidator,
                   ),
 
                   TextFormField(
                     enabled: this._controller.getEditMode(),
-                   // style: TextStyle(color: background_color),
                     obscureText: true,
                     decoration: InputDecoration(
                       icon: Icon(MdiIcons.lock, size: 30, color: decoration_color),
                       labelText: 'Senha',
-                     // labelStyle: TextStyle(color: background_color),
                       errorStyle: TextStyle(color: danger_color),
                     ),
                     initialValue: this._controller.getPassword(),
                     onSaved: (String value){this._controller.setPassword(value);},
-                    validator: this._controller.passwordValidator,
+                    validator: Validators.passwordValidator,
                   ),
                 ],
               ),
